6609296bbeb7d6621ae39bd8692943c1ac3a4aa5
Alba Cárdenas <albillana@MacBook-Pro-de-Alba.local>
2019-04-10 12:43:35 -0400

    Change configuration setting by environment

--- Model/TransbankSdkWebpay.php
+++ Model/TransbankSdkWebpay.php
@@ -16,18 +16,26 @@ class TransbankSdkWebpay {
         if (isset($config)) {
             $environment = isset($config["MODO"]) ? $config["MODO"] : 'INTEGRACION';
             $configuration = Configuration::forTestingWebpayPlusNormal();
-            $configuration = new Configuration();
-            $configuration->setEnvironment($environment);
-            $configuration->setCommerceCode($config["COMMERCE_CODE"]);
-            $configuration->setPrivateKey($config["PRIVATE_KEY"]);
-            $configuration->setPublicCert($config["PUBLIC_CERT"]);
-            if (trim($config["WEBPAY_CERT"]) != '') {
-                $this->log->logDebug($environment. ' - Usando certificado webpay definido por el usuario');
-                $configuration->setWebpayCert($config["WEBPAY_CERT"]);
-            } else {
-                $this->log->logDebug($environment . ' - Usando certificado webpay predeterminado');
-                $configuration->setWebpayCert(Webpay::defaultCert($environment));
+
+            if ($environment != 'INTEGRACION') {
+                $configuration->setEnvironment($environment);
+                $configuration->setPublicCert(Webpay::defaultCert($environment));
+                $configuration->setCommerceCode($config["COMMERCE_CODE"]);
+                $configuration->setPrivateKey($config["PRIVATE_KEY"]);
+                $configuration->setPublicCert($config["PUBLIC_CERT"]);
             }
+            // $configuration = new Configuration();
+            // $configuration->setEnvironment($environment);
+            // $configuration->setCommerceCode($config["COMMERCE_CODE"]);
+            // $configuration->setPrivateKey($config["PRIVATE_KEY"]);
+            // $configuration->setPublicCert($config["PUBLIC_CERT"]);
+            // if (trim($config["WEBPAY_CERT"]) != '') {
+            //     $this->log->logDebug($environment. ' - Usando certificado webpay definido por el usuario');
+            //     $configuration->setWebpayCert($config["WEBPAY_CERT"]);
+            // } else {
+            //     $this->log->logDebug($environment . ' - Usando certificado webpay predeterminado');
+            //     $configuration->setWebpayCert(Webpay::defaultCert($environment));
+            // }
             $this->transaction = (new Webpay($configuration))->getNormalTransaction();
         }
     }
