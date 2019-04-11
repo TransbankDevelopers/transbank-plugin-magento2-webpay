22cba4d97ffd9bac4a67fa4199b9605e4ec2d24d
Alba Cárdenas <albillana@MacBook-Pro-de-Alba.local>
2019-04-11 16:56:11 -0400

    Remove commented code

--- Model/TransbankSdkWebpay.php
+++ Model/TransbankSdkWebpay.php
@@ -24,18 +24,6 @@ class TransbankSdkWebpay {
                 $configuration->setPrivateKey($config["PRIVATE_KEY"]);
                 $configuration->setPublicCert($config["PUBLIC_CERT"]);
             }
-            // $configuration = new Configuration();
-            // $configuration->setEnvironment($environment);
-            // $configuration->setCommerceCode($config["COMMERCE_CODE"]);
-            // $configuration->setPrivateKey($config["PRIVATE_KEY"]);
-            // $configuration->setPublicCert($config["PUBLIC_CERT"]);
-            // if (trim($config["WEBPAY_CERT"]) != '') {
-            //     $this->log->logDebug($environment. ' - Usando certificado webpay definido por el usuario');
-            //     $configuration->setWebpayCert($config["WEBPAY_CERT"]);
-            // } else {
-            //     $this->log->logDebug($environment . ' - Usando certificado webpay predeterminado');
-            //     $configuration->setWebpayCert(Webpay::defaultCert($environment));
-            // }
             $this->transaction = (new Webpay($configuration))->getNormalTransaction();
         }
     }
